e9662da91b84e3385c8b364474114f7a458a8bba
Piscean <106610508+hiimpiscean@users.noreply.github.com>
2022-12-01 23:20:35 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html >
+<head>
+	<title>For my Babi</title>
+	<meta charset="UTF-8">
+	<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="asset/css/style.css">
+</head>
+
+<body>
+
+<div class="wrapper">
+	<div class="container">
+		<div class="main">
+			<section id="testimonials">
+				<div class="text-container">
+					<div class="row">
+						<div class="two columns header-col">
+							<h1><span>This is for you, my little babi boo.</span></h1>
+						</div>
+						<div class="ten columns flex-container">
+							<div class="flexslider">
+								<ul class="slides">
+									<li>
+										<blockquote>
+											<p>
+												Before you came, my life is filled with darkness. Everyday I wake up with a heavy heart and a broken mind.
+											</p>
+											<cite></cite>
+										</blockquote>
+									</li>
+									<li>
+										<blockquote>
+											<p>
+												All of a sudden, you came to me like a guiding star, taking me to a world full of joy and happiness.
+											</p>
+											<cite></cite>
+										</blockquote>
+									</li>
+									<li>
+										<blockquote>
+											<p>
+												Babi, if I gave you my hand, would you take it and make me the happiest man in the world?
+											</p>
+											<cite></cite>
+										</blockquote>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+		<ul class="bg-bubbles">
+			<li><div class="heart pulse2">♥</div></li>
+			<li><div class="heart pulse2">♥</div></li>
+			<li><div class="heart pulse2">♥</div></li>
+			<li><div class="heart pulse2">♥</div></li>
+			<li><div class="heart pulse2">♥</div></li>
+			<li><div class="heart pulse2">♥</div></li>
+			<li><div class="heart pulse2">♥</div></li>
+			<li><div class="heart pulse2">♥</div></li>
+			<li><div class="heart pulse2">♥</div></li>
+			<li><div class="heart pulse2">♥</div></li>
+		</ul>
+	</div>
+</div>
+
+<audio controls autoplay style="display:none;">
+	<source src="asset/audio/David%20Neyrolles%20-%2050%20Boulevard%20Voltaire.mp3" type="audio/mpeg">
+	Your browser does not support the audio element.
+</audio>
+<script>window.jQuery || document.write('<script src="asset/js/jquery-1.10.2.min.js"><\/script>')</script>
+<script src="asset/js/jquery.flexslider.js"></script>
+<script src="asset/js/init.js"></script>
